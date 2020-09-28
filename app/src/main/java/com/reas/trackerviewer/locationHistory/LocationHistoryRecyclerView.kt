c4de8b9cd85b3082330a6fa0a2039da720083c31
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-09-29 00:25:46 +0700

    - Finished Holder class     - Added bind() to bind data and views

--- app/src/main/java/com/reas/trackerviewer/locationHistory/LocationHistoryRecyclerView.kt
+++ app/src/main/java/com/reas/trackerviewer/locationHistory/LocationHistoryRecyclerView.kt
@@ -1,28 +1,41 @@
 package com.reas.trackerviewer.locationHistory
 
 import android.content.Context
+import android.location.Geocoder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.reas.trackerviewer.R
+import java.util.*
 
 class LocationHistoryRecyclerView (
     private val context: Context,
     private val data: ArrayList<LocationBaseObject>): RecyclerView.Adapter<LocationHistoryRecyclerView.Holder>() {
     private val layoutInflater = LayoutInflater.from(context)
+    private val geoCoder = Geocoder(context, Locale.getDefault())
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         return Holder(layoutInflater.inflate(R.layout.recyclerview_location, parent, false))
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(data[position])
     }
 
     override fun getItemCount(): Int = data.size
 
-    class Holder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    inner class Holder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val name = itemView.findViewById<TextView>(R.id.name)
+        val address = itemView.findViewById<TextView>(R.id.address)
+        val mapsIcon = itemView.findViewById<ImageView>(R.id.imageIcon)
 
+        fun bind(location: LocationBaseObject) {
+            val currentAddress = geoCoder.getFromLocation(location.mLatitude, location.mLongitude, 1)
+            name.text = currentAddress[0].featureName ?: ""
+            address.text = currentAddress[0].getAddressLine(0)
+        }
     }
 }
