c1c7f82d7758d92a50c6eee445bfc466fd43b1ab
Raymond Salim <59678005+RaymondSalim@users.noreply.github.com>
2020-09-30 02:51:14 +0700

    - Added toolbar to Chat Activity     - Toolbar title displays conversation name

--- app/src/main/java/com/reas/trackerviewer/messages/chat/ChatActivity.kt
+++ app/src/main/java/com/reas/trackerviewer/messages/chat/ChatActivity.kt
@@ -1,6 +1,7 @@
 package com.reas.trackerviewer.messages.chat
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,15 +22,25 @@ class ChatActivity : AppCompatActivity() {
 
         msgFrom = intent.extras?.get("msgFrom") as String?
 
-
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.chat_toolbar)
+        toolbar.title = msgFrom
+        setSupportActionBar(toolbar!!)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
         initializeRecyclerView()
 
     }
 
-    override fun onBackPressed() {
-        super.onBackPressed()
-        finish()
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return true
     }
 
     private fun initializeRecyclerView() {
